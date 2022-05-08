0ab06f557e92182a8b1958b0f0dc519af8892cb6
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-05-08 15:29:16 +0530

    Setup UI using CardView and hide statusbar

--- app/src/main/java/tech/vinay/quizio/MainActivity.kt
+++ app/src/main/java/tech/vinay/quizio/MainActivity.kt
@@ -1,11 +1,18 @@
 package tech.vinay.quizio
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.statusBars())
+
     }
 }
