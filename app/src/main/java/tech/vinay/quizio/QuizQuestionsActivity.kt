d007108ac305bf815771a9b793c7a639aa9021b2
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2022-05-14 15:53:05 +0530

    design quiz ui

--- app/src/main/java/tech/vinay/quizio/QuizQuestionsActivity.kt
+++ app/src/main/java/tech/vinay/quizio/QuizQuestionsActivity.kt
@@ -2,10 +2,16 @@ package tech.vinay.quizio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        val questionsList = Constants.getQuestions()
+
+        Log.i("Questions Size: ", "${questionsList.size}")
+
     }
 }
